e0c7c95b199b1cf9805f0965a693d5f43e05999f
zsphinxyz <soe.thiha.lwin.5@gmail.com>
2023-11-09 21:07:58 +0630

    ver .1.2.d

--- components/bank/visa.tsx
+++ components/bank/visa.tsx
@@ -7,8 +7,8 @@ const Visa = () => {
 
         <div className="px-3 max-w-[680px] my-10 mx-auto">
             <p className='uppercase text-pop mb-3 text-center md:text-xl'>introduction</p>
-            <h1 className="text-PrimColor text-3xl sm:text-4xl text-center md:text-6xl">Introducing RAFT's Next-Gen Cards</h1>
-            <p className="text-SecColor my-8 text-sm sm:text-lg text-center md:text-xl"> Discover RAFT's latest innovation our new cards. Elevate your banking experience with cutting-edge features, security, and unprecedented convenience.</p>
+            <h1 className="text-PrimColor text-3xl sm:text-4xl text-center md:text-6xl">Introducing RAFT&apos;s Next-Gen Cards</h1>
+            <p className="text-SecColor my-8 text-sm sm:text-lg text-center md:text-xl"> Discover RAFT&apos;s latest innovation our new cards. Elevate your banking experience with cutting-edge features, security, and unprecedented convenience.</p>
         </div>
 
         <div className="flex relative h-[450px] group">
