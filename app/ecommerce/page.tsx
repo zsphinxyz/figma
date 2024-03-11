5e7714ed7d951854c0e8edef0c0ff7735bfcb58b
zsphinxyz <soe.thiha.lwin.5@gmail.com>
2024-03-11 09:54:48 +0630

    credit footer

--- app/ecommerce/page.tsx
+++ app/ecommerce/page.tsx
@@ -9,8 +9,7 @@ import NewsLetter from "@/components/ecommerce/newsletter";
 import Footer from "@/components/ecommerce/footer";
 
 function Page() {
-  
-  // https://www.figma.com/file/Oej5SBUXHdh6DHBwFkmUAC/E-commerce-Website-Template-(Freebie)-(Community)?type=design&node-id=20-2&mode=design&t=JTtD27HalDfe108k-0
+
 
   return (
    
--- components/ecommerce/footer.tsx
+++ components/ecommerce/footer.tsx
@@ -22,18 +22,18 @@ function Footer() {
               <div className="flex w-full">
                 <ul className="basis-1/2 space-y-3">
                   <h1 className="text-2xl space-x-5">Company</h1>
-                  <li className="pl-2 text-black/70">About</li>
-                  <li className="pl-2 text-black/70">Features</li>
-                  <li className="pl-2 text-black/70">Works</li>
-                  <li className="pl-2 text-black/70">Career</li>
+                  <li className="pl-2 text-black/70 hover:text-blue-500 cursor-pointer">About</li>
+                  <li className="pl-2 text-black/70 hover:text-blue-500 cursor-pointer">Features</li>
+                  <li className="pl-2 text-black/70 hover:text-blue-500 cursor-pointer">Works</li>
+                  <li className="pl-2 text-black/70 hover:text-blue-500 cursor-pointer">Career</li>
                 </ul>
                 
                 <ul className="basis-1/2 space-y-3">
                   <h1 className="text-2xl space-x-5">HELP</h1>
-                  <li className="pl-2 text-black/70">Customer Support</li>
-                  <li className="pl-2 text-black/70">Delivery Details</li>
-                  <li className="pl-2 text-black/70">Terms & Conditions</li>
-                  <li className="pl-2 text-black/70">Privacy Policy</li>
+                  <li className="pl-2 text-black/70 hover:text-blue-500 cursor-pointer">Customer Support</li>
+                  <li className="pl-2 text-black/70 hover:text-blue-500 cursor-pointer">Delivery Details</li>
+                  <li className="pl-2 text-black/70 hover:text-blue-500 cursor-pointer">Terms & Conditions</li>
+                  <li className="pl-2 text-black/70 hover:text-blue-500 cursor-pointer">Privacy Policy</li>
                 </ul>
               </div>
 
@@ -41,32 +41,37 @@ function Footer() {
               <div className="flex w-full">
                 <ul className="basis-1/2 space-y-3">
                   <h1 className="text-2xl space-x-5">FAQ</h1>
-                  <li className="pl-2 text-black/70">Account</li>
-                  <li className="pl-2 text-black/70">Manage Deliveries</li>
-                  <li className="pl-2 text-black/70">Orders</li>
-                  <li className="pl-2 text-black/70">Payments</li>
+                  <li className="pl-2 text-black/70 hover:text-blue-500 cursor-pointer">Account</li>
+                  <li className="pl-2 text-black/70 hover:text-blue-500 cursor-pointer">Manage Deliveries</li>
+                  <li className="pl-2 text-black/70 hover:text-blue-500 cursor-pointer">Orders</li>
+                  <li className="pl-2 text-black/70 hover:text-blue-500 cursor-pointer">Payments</li>
                 </ul>
                 
                 <ul className="basis-1/2 space-y-3">
                   <h1 className="text-2xl space-x-5">Resources</h1>
-                  <li className="pl-2 text-black/70">Free eBooks</li>
-                  <li className="pl-2 text-black/70">How to - Blog</li>
-                  <li className="pl-2 text-black/70">Development Tutorial</li>
-                  <li className="pl-2 text-black/70">Youtube Playlist</li>
+                  <li className="pl-2 text-black/70 hover:text-blue-500 cursor-pointer">Free eBooks</li>
+                  <li className="pl-2 text-black/70 hover:text-blue-500 cursor-pointer">How to - Blog</li>
+                  <li className="pl-2 text-black/70 hover:text-blue-500 cursor-pointer">Development Tutorial</li>
+                  <li className="pl-2 text-black/70 hover:text-blue-500 cursor-pointer">Youtube Playlist</li>
                 </ul>
               </div>
             </div>
         </div>
 
-        <div className="text-center mt-5 border-t border-esec py-5 flex flex-col md:flex-row md:justify-between px-3 md:px-5">
-            <p className="text-esec">Shop.co@2000-2023. All Rights Reserved</p>
-            <div className="flex mx-auto mt-3 md:mt-0 md:ml-auto md:mr-0 gap-5">
-              <SiVisa         className="text-4xl w-auto bg-white px-3 py-1 rounded-sm" />
-              <SiMastercard   className="text-4xl w-auto bg-white px-3 py-1 rounded-sm" />
-              <SiPaypal       className="text-4xl w-auto bg-white px-3 py-1 rounded-sm" />
-              <SiApplepay     className="text-4xl w-auto bg-white px-3 py-1 rounded-sm" />
-              <SiGooglepay    className="text-4xl w-auto bg-white px-3 py-1 rounded-sm" />
+        <div className="text-center mt-5 border-t border-esec py-5 flex flex-col gap-3 md:flex-row md:justify-between px-3 md:px-5">
+            <p className="text-esec text-sm">Shop.co@2000-2023. All Rights Reserved</p>
+            <div className="flex mx-auto mt-3 md:mt-0 md:ml-auto md:mr-0 gap-3">
+              <SiVisa         className="text-3xl w-auto bg-white px-3 py-1 hover:bg-black hover:text-white rounded-sm" />
+              <SiMastercard   className="text-3xl w-auto bg-white px-3 py-1 hover:bg-black hover:text-white rounded-sm" />
+              <SiPaypal       className="text-3xl w-auto bg-white px-3 py-1 hover:bg-black hover:text-white rounded-sm" />
+              <SiApplepay     className="text-3xl w-auto bg-white px-3 py-1 hover:bg-black hover:text-white rounded-sm" />
+              <SiGooglepay    className="text-3xl w-auto bg-white px-3 py-1 hover:bg-black hover:text-white rounded-sm" />
             </div>
+            <p className="text-sm">     
+              Credit: <a className="text-blue-500 underline" href=" https://www.figma.com/community/file/1273571982885059508/e-commerce-website-template-freebie?searchSessionId=ltmce3zt-740clhvuuof" target="_blank" rel="noopener noreferrer">Figma Project </a>
+              by <a className="text-blue-500 underline" href="https://www.figma.com/@hamzauix" target="_blank" rel="noopener noreferrer">Hamza Naeem</a>
+            </p>
+            
         </div>
     </footer>
   )
